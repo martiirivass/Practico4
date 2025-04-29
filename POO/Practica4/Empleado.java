dfd3834a8ceba0a0d9c871eb300ae50705905ebd
martiirivass <martiirivas2004@gmail.com>
2025-04-28 22:59:00 -0300

    Agrego ejercicios resueltos

---
+++ POO/Practica4/Empleado.java
@@ -0,0 +1,102 @@
+package POO.Practica4;
+
+public class Empleado {
+    private String nombre, puesto;
+    private int id; //identificador unico
+    private double salario;
+    static int totalEmpleados; //contador que registra la cantidad de empleados creados
+
+
+    //Constructor
+    public Empleado(String nombre, String puesto) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = 100000.00;
+        totalEmpleados ++;  //aumenta el contador en 1
+        this.id = totalEmpleados;  //Se le dará el número de totalEmpleados
+    }
+    //Sobrecarga de constructor con parámetros
+    public Empleado(String nombre, String puesto, int id, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.id = id;
+        this.salario = salario;
+        totalEmpleados++;  //aumenta el contador en 1
+    }
+
+    //GETTERS AND SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public static int getTotalEmpleados() {
+        return totalEmpleados;
+    }
+
+    //METODO TOTAL EMPLEADOS
+    public static void setTotalEmpleados(int totalEmpleados) {
+        Empleado.totalEmpleados = totalEmpleados;
+    }
+
+    //METODOS ACTUALIZAR SALARIO
+    public double actualizarSalario(int porcentajeAumento){
+        if (porcentajeAumento>0) {
+            this.salario = (this.salario * (1 + (porcentajeAumento/100.00)));
+        } else System.out.println("El porcentaje de aumento debe ser mayor de aumento");
+        return this.salario;
+    }
+
+    public double actualizarSalario(double cantidadFija){
+
+        if (cantidadFija > 0) {
+            this.salario += cantidadFija;
+        } else System.out.println("La cantidad debe ser mayor a 0");
+        return this.salario;
+    }
+
+    //METODO TOSTRING
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", puesto='" + puesto + '\'' +
+                ", id=" + id +
+                ", salario=" + salario +
+                '}';
+    }
+
+    public static int mostrarEmpleados(){
+        return totalEmpleados;
+    }
+
+}
+
+
